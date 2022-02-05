9905734209c56ad9b6d893b380bb6d0e22278af1
dladncks1217 <dlaxodud1217@gmail.com>
2022-02-05 20:22:42 +0900

    add async actions

---
+++ front/actions/index.js
@@ -0,0 +1,60 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8000";
+
+export const addProblemAction = createAsyncThunk(
+  "problem/update",
+  async (data, ThunkAPI) => {
+    console.log(data);
+    const result = axios.post("/year/add", {
+      problem: data.name,
+      problemtype: data.type,
+      year: data.year,
+      solvedate: data.date,
+      success: data.isCorrect,
+      review: data.review,
+    });
+    return result;
+  }
+);
+
+export const get2021ProblemAction = createAsyncThunk(
+  "problem/2021",
+  async (data, ThunkAPI) => {
+    const result = axios.post("/year/2021");
+    return result;
+  }
+);
+
+export const get2022ProblemAction = createAsyncThunk(
+  "problem/2022",
+  async (data, ThunkAPI) => {
+    const result = axios.post("/year/2022");
+    return result;
+  }
+);
+
+export const getReviewedProblemAction = createAsyncThunk(
+  "problem/reviewed",
+  async (data, ThunkAPI) => {
+    const result = axios.post("/year/reviewed");
+    return result;
+  }
+);
+
+export const getNotReviewedProblemAction = createAsyncThunk(
+  "problem/notreviewed",
+  async (data, ThunkAPI) => {
+    const result = axios.post("/year/notreviewed");
+    return result;
+  }
+);
+
+export const getTypeProblemAction = createAsyncThunk(
+  "problem/type",
+  async (data, ThunkAPI) => {
+    const result = axios.post(`/year/typecheck/${data.type}`);
+    return result;
+  }
+);
