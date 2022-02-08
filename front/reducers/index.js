6c33d3db2ef200ada79d47ad2483def330133b05
dladncks1217 <dlaxodud1217@gmail.com>
2022-02-08 22:58:15 +0900

    add reducers

--- front/reducers/index.js
+++ front/reducers/index.js
@@ -1,6 +1,18 @@
 import { combineReducers } from "redux";
-import problemSlice from "./problems";
+import {
+  addProblemSlice,
+  problem2021Slice,
+  problem2022Slice,
+  getNotReviewedProblemSlice,
+  getReviewedProblemSlice,
+  getTypeProblemSlice,
+} from "./problems";
 
 export default combineReducers({
-  problem: problemSlice.reducer,
+  addProblem: addProblemSlice.reducer,
+  get2021: problem2021Slice.reducer,
+  get2022: problem2022Slice.reducer,
+  notreviewed: getNotReviewedProblemSlice.reducer,
+  reviewed: getReviewedProblemSlice.reducer,
+  typeProblem: getTypeProblemSlice.reducer,
 });
--- front/reducers/problems.js
+++ front/reducers/problems.js
@@ -14,6 +14,9 @@ export const initialState = {
   problemAdding: false,
   problemAdded: false,
   problemAddErrorReason: null,
+  problemLoading: false,
+  problemLoaded: false,
+  problemLoadErrorReason: null,
 };
 
 export const addProblemSlice = createSlice({
@@ -23,6 +26,7 @@ export const addProblemSlice = createSlice({
   extraReducers: {
     [addProblemAction.pending](state, action) {
       state.problemAdding = true;
+      state.problemAdded = false;
     },
     [addProblemAction.fulfilled](state, action) {
       state.problemAdded = true;
@@ -31,6 +35,106 @@ export const addProblemSlice = createSlice({
     [addProblemAction.rejected](state, action) {
       state.problemAdding = false;
       state.problemAddErrorReason = action.error;
+    },
+  },
+});
+
+export const problem2021Slice = createSlice({
+  name: "2021Problem",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [get2021ProblemAction.pending](state, action) {
+      state.problemLoaded = false;
+      state.problemLoading = true;
+    },
+    [get2021ProblemAction.pending](state, action) {
+      state.problemLoading = false;
+      state.problemLoaded = true;
+    },
+    [get2021ProblemAction.pending](state, action) {
+      state.problemLoaded = false;
+      state.problemLoadErrorReason = action.error;
+    },
+  },
+});
+
+export const problem2022Slice = createSlice({
+  name: "2022Problems",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [get2022ProblemAction.pending](state, action) {
+      state.problemLoaded = false;
+      state.problemLoading = true;
+    },
+    [get2022ProblemAction.pending](state, action) {
+      state.problemLoading = false;
+      state.problemLoaded = true;
+    },
+    [get2022ProblemAction.pending](state, action) {
+      state.problemLoaded = false;
+      state.problemLoadErrorReason = action.error;
+    },
+  },
+});
+
+export const getNotReviewedProblemSlice = createSlice({
+  name: "notReviewedProblems",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getNotReviewedProblemAction.pending](state, action) {
+      state.problemLoaded = false;
+      state.problemLoading = true;
+    },
+    [getNotReviewedProblemAction.pending](state, action) {
+      state.problemLoading = false;
+      state.problemLoaded = true;
+    },
+    [getNotReviewedProblemAction.pending](state, action) {
+      state.problemLoaded = false;
+      state.problemLoadErrorReason = action.error;
+    },
+  },
+});
+
+export const getReviewedProblemSlice = createSlice({
+  name: "reviewedProblems",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getReviewedProblemAction.pending](state, action) {
+      state.problemLoaded = false;
+      state.problemLoading = true;
+    },
+    [getReviewedProblemAction.pending](state, action) {
+      state.problemLoading = false;
+      state.problemLoaded = true;
+    },
+    [getReviewedProblemAction.pending](state, action) {
+      state.problemLoaded = false;
+      state.problemLoadErrorReason = action.error;
+    },
+  },
+});
+
+export const getTypeProblemSlice = createSlice({
+  name: "typeProblems",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getTypeProblemAction.pending](state, action) {
+      state.problemLoaded = false;
+      state.problemLoading = true;
+    },
+    [getTypeProblemAction.pending](state, action) {
+      state.problemLoading = false;
+      state.problemLoaded = true;
+    },
+    [getTypeProblemAction.pending](state, action) {
+      state.problemLoaded = false;
+      state.problemLoadErrorReason = action.error;
     },
   },
 });
